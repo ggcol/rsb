a840becea1937a362d0d85f35fcc0f6c2c5445e3
Gianluca Colombo <gianluca.colombo@lobra.com>
2024-06-19 15:17:37 +0200

    rsb internals visibility to playground

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using rsb;
+using rsb.Workers;
+
+var sagaWorker = new SagaWorker<MySaga, MySagaData>(new MySaga());
+
+
+public class MySaga : Saga<MySagaData>, IAmStartedBy<InitMessage>
+{
+    public Task Handle(InitMessage message, IMessagingContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class MySagaData
+{
+    
+}
+
+public class InitMessage{}
