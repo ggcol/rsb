c31220db81ec1d4192643a6996b781ca9e36d500
Gianluca Colombo <gianluca.colombo@lobra.com>
2024-07-12 18:01:47 +0200

    new listener approach init

--- Rsb/IHandleMessage.cs
+++ Rsb/IHandleMessage.cs
@@ -1,11 +1,26 @@
-﻿namespace Rsb;
+﻿using Rsb.Configurations;
+
+namespace Rsb;
 
 public interface IHandleMessage<in TMessage>
+    // : IHandleMessage 
+    where TMessage : IAmAMessage
 {
-    public Task Handle(TMessage message, IMessagingContext context);
+    //TODO nullable?
+    public Task Handle(TMessage? message, IMessagingContext context);
     
     public Task HandleErrors(Exception ex, IMessagingContext context)
     {
         throw ex;
     }
 }
+
+// public interface IHandleMessage
+// {
+//     public Task Handle(IAmAMessage message, IMessagingContext context);
+//     
+//     public Task HandleErrors(Exception ex, IMessagingContext context)
+//     {
+//         throw ex;
+//     }
+// }
