d897982d344a8fb0453e4327c347b45603c872a1
Gianluca Colombo <gianluca.colombo@lobra.com>
2024-07-16 15:41:42 +0200

    Development utils (delete queue/topics programmatically)

---
+++ DevelopmentUtils/Program.cs
@@ -0,0 +1,19 @@
+﻿using Azure.Messaging.ServiceBus.Administration;
+
+var connectionString 
+    = "";
+var adminClient = new ServiceBusAdministrationClient(connectionString);
+
+// Delete all queues
+var queues = adminClient.GetQueuesAsync();
+await foreach (var queue in queues)
+{
+    await adminClient.DeleteQueueAsync(queue.Name);
+}
+
+// Delete all topics
+var topics = adminClient.GetTopicsAsync();
+await foreach (var topic in topics)
+{
+    await adminClient.DeleteTopicAsync(topic.Name);
+}
