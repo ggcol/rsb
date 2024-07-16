857a1e0414b2022c1aa7db1d2606ff6971032ffc
Gianluca Colombo <gianluca.colombo@lobra.com>
2024-07-16 16:06:56 +0200

    DependencyInjection refactoring

--- Rsb/Core/DependencyInjection.cs
+++ Rsb/Core/DependencyInjection.cs
@@ -22,47 +22,59 @@ public static class DependencyInjection
                         hostBuilderContext.Configuration.GetSection(typeof(TSettings).Name));
 
                 services
-                    .AddMemoryCache()
-                    .AddSingleton<IAzureServiceBusService,
-                        AzureServiceBusService<TSettings>>()
-                    .AddTransient<IMessagingContext, MessagingContext>()
-                    .AddScoped<IMessageEmitter, MessageEmitter>();
-
-                var handlersTypes =
-                    AssemblySearcher.GetIHandleMessageImplementersTypes(
-                        Assembly.GetEntryAssembly());
-
-                var messageTypes =
-                    AssemblySearcher.GetIHandleMessageImplementersMessageTypes(
-                        handlersTypes);
-
-
-                foreach (var messageType in messageTypes)
-                {
-                    var handlerType =
-                        AssemblySearcher
-                            .GetIHandleMessageImplementerByMessageType(
-                                handlersTypes, messageType);
-
-                    var handlerServiceType = typeof(IHandleMessage<>)
-                        .MakeGenericType(messageType);
-
-                    //TODO this may be transient?
-                    //add handlers as services
-                    services.AddScoped(handlerServiceType, handlerType);
-
-                    var brokerServiceType =
-                        typeof(IListenerBroker<>).MakeGenericType(messageType);
-
-                    var brokerImplType =
-                        typeof(ListenerBroker<>).MakeGenericType(messageType);
-
-                    //TODO this may be transient?
-                    //add brokers as services, handlers are then injected
-                    services.AddScoped(brokerServiceType, brokerImplType);
-                }
+                    .AddMandatoryServices<TSettings>()
+                    .RegisterHandlers();
 
                 services.AddHostedService<RsbWorker>();
             });
+    }
+
+    private static IServiceCollection AddMandatoryServices<TSettings>(this IServiceCollection services)
+        where TSettings : class, IConfigureAzureServiceBus, new()
+    {
+        return services
+            .AddMemoryCache()
+            .AddSingleton<IAzureServiceBusService, AzureServiceBusService<TSettings>>()
+            .AddScoped<IMessagingContext, MessagingContext>()
+            .AddScoped<IMessageEmitter, MessageEmitter>();
+    }
+    
+    private static IServiceCollection RegisterHandlers(this IServiceCollection services)
+    {
+        var handlersTypes =
+            AssemblySearcher.GetIHandleMessageImplementersTypes(
+                Assembly.GetEntryAssembly());
+
+        var messageTypes =
+            AssemblySearcher.GetIHandleMessageImplementersMessageTypes(
+                handlersTypes);
+
+
+        foreach (var messageType in messageTypes)
+        {
+            var handlerType =
+                AssemblySearcher
+                    .GetIHandleMessageImplementerByMessageType(
+                        handlersTypes, messageType);
+
+            var handlerServiceType = typeof(IHandleMessage<>)
+                .MakeGenericType(messageType);
+
+            //TODO this may be transient?
+            //add handlers as services
+            services.AddScoped(handlerServiceType, handlerType);
+
+            var brokerServiceType =
+                typeof(IListenerBroker<>).MakeGenericType(messageType);
+
+            var brokerImplType =
+                typeof(ListenerBroker<>).MakeGenericType(messageType);
+
+            //TODO this may be transient?
+            //add brokers as services, handlers are then injected
+            services.AddScoped(brokerServiceType, brokerImplType);
+        }
+
+        return services;
     }
 }
