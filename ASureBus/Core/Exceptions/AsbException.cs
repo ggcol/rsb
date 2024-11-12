f60f5c43774d8e9cb60d902ae7df59f4afc4bd5a
Gianluca Colombo <gianluca.colombo@lobra.com>
2024-11-12 10:33:52 +0100

    AsbException

---
+++ ASureBus/Core/Exceptions/AsbException.cs
@@ -0,0 +1,7 @@
+﻿namespace ASureBus.Core.Exceptions;
+
+internal sealed class AsbException : Exception
+{
+    internal required Exception OriginalException { get; init; }
+    internal required Guid CorrelationId { get; init; }
+}
