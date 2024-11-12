b5d63d720f3e8e4c4845c5c09101d12dceae8a20
Gianluca Colombo <gianluca.colombo@lobra.com>
2024-11-12 10:34:12 +0100

    SagaNotFoundException

---
+++ ASureBus/Core/Exceptions/SagaNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace ASureBus.Core.Exceptions;
+
+internal sealed class SagaNotFoundException : Exception
+{
+    internal SagaNotFoundException(Type sagaType, Guid correlationId)
+        : base($"Saga of type {sagaType.Name} with correlation id {correlationId} not found")
+    { }
+}
