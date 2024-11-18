60fe20c28b2213ed0fdd0f8eabe6e959a5400e8f
Gianluca Colombo <gianluca.colombo@lobra.com>
2024-11-18 16:04:05 +0100

    ConfigurationNullException tests

---
+++ ASureBus.Tests/ASureBus/ConfigurationObjects/Exceptions/ConfigurationNullExceptionTests.cs
@@ -0,0 +1,35 @@
+﻿using ASureBus.ConfigurationObjects.Exceptions;
+
+namespace ASureBus.Tests.ASureBus.ConfigurationObjects.Exceptions;
+
+[TestFixture]
+public class ConfigurationNullExceptionTests
+{
+    [Test]
+    public void Constructor_ShouldSetMessageCorrectly()
+    {
+        // Arrange
+        const string configName = "TestConfig";
+
+        // Act
+        var exception = new ConfigurationNullException(configName);
+
+        // Assert
+        Assert.That(exception.Message,
+            Is.EqualTo($"Configuration object '{configName}' cannot be null."));
+    }
+
+    [Test]
+    public void Constructor_ShouldSetConfigNameCorrectly()
+    {
+        // Arrange
+        const string configName = "AnotherConfig";
+
+        // Act
+        var exception = new ConfigurationNullException(configName);
+
+        // Assert
+        Assert.That(exception.Message,
+            Is.EqualTo($"Configuration object '{configName}' cannot be null."));
+    }
+}
